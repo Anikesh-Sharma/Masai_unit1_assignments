5406495bf3f38100952807ce6597a0b2ee4a61a4
Anikesh Sharma <91007286+Anikesh-Sharma@users.noreply.github.com>
2024-04-15 15:56:16 +0530

    Create Number.py

---
+++ Number.py
@@ -0,0 +1,11 @@
+# initializing the number and assigning value 10
+number = 10
+
+# printing the variable 'number'
+print(number)
+
+# Assigning the value 20
+number = 20
+
+# Printing the variable with new value
+print(number)
