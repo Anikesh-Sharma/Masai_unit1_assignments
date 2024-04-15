5fca5eba699a88511e2f776c78cee5e883277d76
Anikesh Sharma <91007286+Anikesh-Sharma@users.noreply.github.com>
2024-04-15 16:02:27 +0530

    Add files via upload

---
+++ Details.py
@@ -0,0 +1,9 @@
+# Assigning values in variable
+name = 'Anikesh Sharma'
+age = 24
+
+# printing the value name, age in the same line
+print(name, age)
+
+# printing the type of datatype used while creating the code
+print(type(name), type(age))
